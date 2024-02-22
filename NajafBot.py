a8bf5a9ae7c4b716c3a38937f8688f4d5751b3bd
Najaf Ali <65591681+najafali14@users.noreply.github.com>
2024-02-22 19:00:44 +0500

    Create NajafBot.py

---
+++ NajafBot.py
@@ -0,0 +1,31 @@
+#NAJAFALI
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+genai.configure(api_key='AIzaSyDZVaeCYR5DeUwGOvFJWhapQ-b7psA5eIk')
+model = genai.GenerativeModel('gemini-pro')
+# %%time
+
+import streamlit as st
+
+message = st.chat_input('Enter your prompt...')
+if message:
+  st.write('You: ',message)
+
+  response = model.generate_content(f'Hello {message}')
+
+  rep = response.text
+  st.write('NajafBot: ',rep)
+else:
+  pass
+
+
+# if __name__ == "__main__":
